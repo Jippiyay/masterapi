f889281b1c41fe94b5998a62a7a55e31988d817f
Jippiyay <johanna.giesecke@web.de>
2023-03-23 22:31:53 +0100

    initial commit

---
+++ R/api.r
@@ -0,0 +1,51 @@
+#' Saves object to database
+#'
+#' This function saves to the database behind the masters project api
+#'
+#' @param obj Object to save
+#' @return NULL
+#' @export
+store <- function(obj) {
+    tcres = tryCatch({
+        apiurl <- Sys.getenv("APIURL")
+        print(apiurl)
+        res <- POST(apiurl + "/shiny-data", body = obj, encode = "json", verbose())
+        print(res)
+        if(res$status_code == 200){
+          showNotification(duration = 5, "Prognose wurde gespeichert")
+        }
+        else{
+          showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "error")
+        }
+      }, warning = function(w) {
+        showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "warning")
+      }, error = function(e) {
+        showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nWende dich bitte an Johanna.", type = "error")
+    })
+}
+
+#' Tracks events object to database
+#'
+#' This function saves to the database behind the masters project api
+#'
+#' @param obj Object to save
+#' @return NULL
+#' @export
+track <- function(event) {
+  tcres = tryCatch({
+        apiurl <- Sys.getenv("APIURL")
+        print(apiurl)
+        res <- POST(apiurl + "/event", body = obj, encode = "json", verbose())
+        print(res)
+        if(res$status_code == 200){
+          # showNotification(duration = 5, "Prognose wurde gespeichert")
+        }
+        else{
+          # showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "error")
+        }
+      }, warning = function(w) {
+        # showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "warning")
+      }, error = function(e) {
+        # showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nWende dich bitte an Johanna.", type = "error")
+  })
+}
