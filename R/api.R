876c2f3bcb1a67254ad85a36f9e1b26597178478
Jippiyay <johanna.giesecke@web.de>
2023-03-23 23:03:57 +0100

    added package names to namespace and package

--- R/api.R
+++ R/api.R
@@ -4,23 +4,23 @@
 #'
 #' @param obj Object to save
 #' @return NULL
-#' @export
+#' @export store
 store <- function(obj) {
     tcres = tryCatch({
         apiurl <- Sys.getenv("APIURL")
         print(apiurl)
-        res <- POST(apiurl + "/shiny-data", body = obj, encode = "json", verbose())
+        res <- httr::POST(apiurl + "/shiny-data", body = obj, encode = "json", verbose())
         print(res)
         if(res$status_code == 200){
-          showNotification(duration = 5, "Prognose wurde gespeichert")
+          shiny::showNotification(duration = 5, "Prognose wurde gespeichert")
         }
         else{
-          showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "error")
+          shiny::showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "error")
         }
       }, warning = function(w) {
-        showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "warning")
+        shiny::showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nVersuche es bitte erneut.", type = "warning")
       }, error = function(e) {
-        showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nWende dich bitte an Johanna.", type = "error")
+        shiny::showNotification(duration = 5,"Prognose konnte nicht gespeichert werden.\nWende dich bitte an Johanna.", type = "error")
     })
 }
 
@@ -28,9 +28,9 @@ store <- function(obj) {
 #'
 #' This function saves to the database behind the masters project api
 #'
-#' @param obj Object to save
+#' @param event Object to save
 #' @return NULL
-#' @export
+#' @export track
 track <- function(event) {
   tcres = tryCatch({
         apiurl <- Sys.getenv("APIURL")
